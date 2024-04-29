1b67586dd6a36f4712d37b7d5c180eda0f057244
Ankush Ladani <astarladani@gmail.com>
2024-04-29 21:18:13 +0530

    Home Page Started

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,5 +1,35 @@
-import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import messages from "@/messages.json";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function Home() {
-  return <h1 className="text-center text-5xl">Mystery App me Swagat hai</h1>;
+  return (
+    <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
+      <section className="text-center mb-8 md:mb-12">
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Dive into the World of Anonymous Feedback
+        </h1>
+        <p className="mt-3 md:mt-4 text-base md:text-lg">
+          True Feedback - Where your identity remains a secret.
+        </p>
+      </section>
+
+      <Carousel>
+        <CarouselContent>
+          {messages.map((message) => {
+            return <CarouselItem key={message.title}></CarouselItem>;
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </main>
+  );
 }
