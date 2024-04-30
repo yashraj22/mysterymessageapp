92170e00d1205554ca3b07ba1a4bc5689dbd5fb1
yashraj22 <yyashraj05@gmail.com>
2024-04-30 10:07:37 +0530

    [BUG FIX ] Update middleware to exclude "/dashboard" route

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,7 +13,7 @@ export async function middleware(request: NextRequest) {
             url.pathname.startsWith("/sign-in") || 
             url.pathname.startsWith("/sign-up") || 
             url.pathname.startsWith("/verify") || 
-            url.pathname.startsWith("/")
+            (url.pathname.startsWith("/") && !url.pathname.startsWith("/dashboard"))
         )
 
     ){
